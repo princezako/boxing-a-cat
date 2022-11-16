fc4ab05ef55f7b80af1fd54362ff9f80442439e0
Elizabeth Bonnett <ZJAP031@live.rhul.ac.uk>
2022-11-16 17:34:46 +0000

    update command line

--- src/example_package/command_line_interface.py
+++ src/example_package/command_line_interface.py
@@ -5,6 +5,8 @@ from example_package.boxing_cat_solver import boxing_cat_solver
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("number", help="The number to add one too", type=float)
+    parser.add_argument("string", help="Your chosen potential", type=string)
     args = parser.parse_args()
-    print(f"{args.number} add one is {add_one(args.number)}")
+    print(f"{args.string} applied to the potential is 
+
+

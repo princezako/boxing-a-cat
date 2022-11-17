18918fe73ef41880698b5b79780ef7e063eee449
Elizabeth Bonnett <ZJAP031@live.rhul.ac.uk>
2022-11-17 14:38:52 +0000

    deleted example test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,15 +1,32 @@
-from example_package.example import add_one
+import boxing_cat_solver as m
 import pytest
 
+ 
 
-def test_add_one_works():
-    " Check that the method work as expected"
-    number = 1
-    assert number + 1 == add_one(number)
+Example = m.Environment()
+Example.Write_potential("np.sin(x)")
+
+ 
+
+def test_WritePotential_works():
+    "Check that the code works as expected"
+    string = "x"
+    Example.Write_potential(string)
+    assert Example.potential.any() == Example.x_array.any()
 
 
-def test_add_one_fails():
-    " Check that the method fails when it is given a string "
-    number = "str"
+
+def test_WritePotential_fails_1():
+    
+    string = "float"
+    Example.Write_potential(string)
     with pytest.raises(TypeError):
-        add_one(number)
+         Example.Write_potential(string)
+        
+def test_WritePotential_fails_2():
+    
+    string = 3
+    Example.Write_potential(string)
+    with pytest.raises(TypeError):
+         Example.Write_potential(string)
+        

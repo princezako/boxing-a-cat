e2b436d3ac2b63fb14537c83d7b15aed90e9faad
Elizabeth Bonnett <ZJAP031@live.rhul.ac.uk>
2022-11-17 12:17:24 +0000

    getting command line to work

--- src/example_package/command_line_interface.py
+++ src/example_package/command_line_interface.py
@@ -1,12 +1,12 @@
 import argparse
 
-from example_package.boxing_cat_solver import boxing_cat_solver
+from example_package.example import add_one
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("string", help="Your chosen potential", type=string)
+    parser.add_argument("number", help="The number to add one too", type=float)
     args = parser.parse_args()
-    print(f"{args.string} applied to the potential is 
+    print(f"{args.number} add one is {add_one(args.number)}")
 
 
